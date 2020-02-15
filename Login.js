83bd559746bb84b25989377e040c7499cf4da952
Jialing Tong <jialingt@usc.edu>
2020-02-15 05:54:46 -0800

    Initial Login

---
+++ Login.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { Alert, Button, TextInput, View, StyleSheet} from 'react-native';
+
+export default class App extends Component {
+    constructor(props) {
+      super(props);
+      
+      this.state = {
+        username: '',
+        password: '',
+      };
+    }
+    
+    onLogin() {
+      const { username, password } = this.state;
+  
+      Alert.alert('Credentials', `${username} + ${password}`);
+    }
+  
+    onRegister(){
+      
+    }
+  
+    render() {
+      return (
+        <View style={styles.container}>
+          <TextInput
+            value={this.state.username}
+            onChangeText={(username) => this.setState({ username })}
+            placeholder={'Username'}
+            style={styles.input}
+          />
+          <TextInput
+            value={this.state.password}
+            onChangeText={(password) => this.setState({ password })}
+            placeholder={'Password'}
+            secureTextEntry={true}
+            style={styles.input}
+          />
+          
+          <Button
+            title={'Login'}
+            style={styles.input}
+            onPress={this.onLogin.bind(this)}
+          />
+          <Button
+            title={'Create a new account'}
+            style={styles.input}
+            onPress={this.onRegister()}
+          />
+        </View>
+      );
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#ecf0f1',
+    },
+    input: {
+      width: 200,
+      height: 44,
+      padding: 10,
+      borderWidth: 1,
+      borderColor: 'black',
+      marginBottom: 10,
+    },
+  });
+  
+
